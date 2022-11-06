1eb8121fc68d77d3aee8861fe7474f83bf938c44
Vlad Riscutia <vladris@outlook.com>
2022-11-06 15:07:59 -0800

    Add signing using crypto.subtle

---
+++ packages/cryptography/src/signing.ts
@@ -0,0 +1,83 @@
+// Digital signing using crypto.subtle
+import { encode, decode } from "base64-arraybuffer";
+
+// Keys are represented as strings
+type Key = string;
+
+// Public/private key pair
+type PublicPrivateKeyPair = {
+    publicKey: Key;
+    privateKey: Key;
+};
+
+namespace Signing {
+    // Generate public/private key pair
+    export async function generatePublicPrivateKeyPair(): Promise<PublicPrivateKeyPair> {
+        const subtle = crypto.subtle;
+        const keys = await subtle.generateKey(
+            {
+                name: "rsa-oaep",
+                modulusLength: 4096,
+                publicExponent: new Uint8Array([1, 0, 1]),
+                hash: "sha-256",
+            },
+            true,
+            ["encrypt", "decrypt"]
+        );
+
+        return {
+            publicKey: encode(await subtle.exportKey("spki", keys.publicKey)),
+            privateKey: encode(
+                await subtle.exportKey("pkcs8", keys.privateKey)
+            ),
+        };
+    }
+
+    // Returns payload signature
+    export async function sign(
+        payload: string,
+        privateKey: Key
+    ): Promise<string> {
+        const subtle = crypto.subtle;
+
+        const pk = await subtle.importKey(
+            "pkcs8",
+            decode(privateKey),
+            { name: "RSA-PSS", hash: "SHA-256" },
+            true,
+            ["sign"]
+        );
+
+        return encode(
+            await subtle.sign(
+                { name: "RSA-PSS", saltLength: 256 },
+                pk,
+                decode(payload)
+            )
+        );
+    }
+
+    // Verify payload signature
+    export async function verifySignature(
+        payload: string,
+        signature: string,
+        publicKey: Key
+    ): Promise<boolean> {
+        const subtle = crypto.subtle;
+
+        const pk = await subtle.importKey(
+            "spki",
+            decode(publicKey),
+            { name: "RSA-PSS", hash: "SHA-256" },
+            true,
+            ["verify"]
+        );
+
+        return subtle.verify(
+            { name: "RSA-PSS", saltLength: 256 },
+            pk,
+            decode(signature),
+            decode(payload)
+        );
+    }
+}
