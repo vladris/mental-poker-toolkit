2f0ae5438d2e5a046dd3f5d802473907eceeba6f
Vlad Riscutia <vladris@outlook.com>
2022-12-15 11:35:15 -0800

    Add comments

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,6 +1,9 @@
 // TODO: refactor this into multiple files
+
+// Client ID
 export type ClientId = string;
 
+// Transport interface
 export declare interface ITransport<T> {
     postAction(value: T): Promise<void>;
 
